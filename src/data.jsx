5d1760605a4dc50ed534b10aa944e3aa26e08a52
Wanem808 <kalidashlimbu3@gmail.com>
2023-08-22 14:48:19 +0545

    first commit

---
+++ src/data.jsx
@@ -0,0 +1,36 @@
+const people = [
+  {
+    id: 1,
+    image: "https://st4.depositphotos.com/21607914/23444/i/1600/depositphotos_234445476-stock-photo-lionel-messi-argentina-looks-group.jpg",
+    name: "Lionel Messi",
+    title: "Right Winger",
+    quote:
+      "Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.",
+  },
+  {
+    id: 2,
+    image: "https://st.depositphotos.com/1837549/1885/i/450/depositphotos_18851705-stock-photo-cristiano-ronaldo-of-real-madrid.jpg",
+    name: "Cristiano Ronaldo",
+    title: "Striker",
+    quote:
+      "Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.",
+  },
+  {
+    id: 3,
+    image: "https://st3.depositphotos.com/2810819/17812/i/450/depositphotos_178122888-stock-photo-istanbul-turkey-december-2017-wax.jpg",
+    name: "Neymar Jr",
+    title: "Left Winger",
+    quote:
+      "Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.",
+  },
+  {
+    id: 4,
+    image: "https://st5.depositphotos.com/8132070/65274/i/600/depositphotos_652746726-stock-photo-germany-dortmund-april-2023-jude.jpg",
+    name: "Jude Bellingham",
+    title: "Midfielder",
+    quote:
+      "Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ",
+  },
+];
+
+export default people;
